682042be384c30c7bcd74f523d75cf490c97e319
Rapha Taylor <raphataylor1@gmail.com>
2024-01-22 16:27:01 +0000

    Added Hyperlinks to views and updated URL mapping

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	return HttpResponse("Rango says hey there partner!\nCheck out <a href='/rango/about/'>About</a>")
+
+def about(request):
+	return HttpResponse("Rango says here is the about page.\nCheck out <a href='/rango/'>Index</a>")

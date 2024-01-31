3a18fbd10c53dd367ebe37bfa500090312940e31
Rapha Taylor <raphataylor1@gmail.com>
2024-01-31 20:20:09 +0000

    Update populate_rango.py

--- populate_rango.py
+++ populate_rango.py
@@ -53,7 +53,7 @@ def add_page(cat, title, url, views=0):
     return p
 
 def add_cat(name):
-    c = Category.objects.get_or_create(name)[0]
+    c = Category.objects.get_or_create(name=name)[0]
     c.save()
     return c
 
